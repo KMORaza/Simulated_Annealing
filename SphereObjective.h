372551515c58813c9786e05c325fcb1c41bbba5c
Raza <141234596+KMORaza@users.noreply.github.com>
2024-01-22 02:39:33 +0530

    -

--- SphereObjective.h
+++
@@ -1,7 +0,0 @@
-// Created: September 2022
-#pragma once
-#include "ObjectiveFunction.h"
-class SphereObjective : public ObjectiveFunction {
-public:
-    double evaluate(const double* solution, int size) override;
-};
