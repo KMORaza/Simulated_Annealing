9eb8ee44fe29de9057131a8530c9c3a1d876cc84
Raza <141234596+KMORaza@users.noreply.github.com>
2024-01-22 02:37:59 +0530

    -

--- ObjectiveFunction.h
+++
@@ -1,7 +0,0 @@
-// Created: September 2022
-#pragma once
-class ObjectiveFunction {
-public:
-    virtual double evaluate(const double* solution, int size) = 0;
-    virtual ~ObjectiveFunction() {}
-};
