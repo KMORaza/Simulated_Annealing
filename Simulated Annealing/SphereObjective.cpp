f2068c612d8a2e0a1f99be3ce9e82fbf649973a4
Raza <141234596+KMORaza@users.noreply.github.com>
2024-01-22 16:51:44 +0530

    -

--- Simulated Annealing/SphereObjective.cpp
+++ Simulated Annealing/SphereObjective.cpp
@@ -1,6 +1,5 @@
-// Create
+// Created: September 2022
 #include "SphereObjective.h"
-
 double SphereObjective::evaluate(const double* solution, int size) {
     double sum = 0.0;
     for (int i = 0; i < size; ++i) {
